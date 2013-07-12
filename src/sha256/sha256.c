b020f09734e63ec31a19f13ab2278140f4614af3
fpgaminer <fpgaminer@bitcoin-mining.com>
2013-07-11 23:36:55 -0700

    Fixed compiler warning in sha256.c

--- src/sha256/sha256.c
+++ src/sha256/sha256.c
@@ -103,7 +103,7 @@ void SHA256 (uint8_t *hash, uint8_t const * msg, uint32_t len)
 {
 	uint32_t digest[8] = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
 	
-	for (int i = 0; i < (len >> 6); ++i)
+	for (uint32_t i = 0; i < (len >> 6); ++i)
 	{
 		uint32_t chunk[16];
 		
