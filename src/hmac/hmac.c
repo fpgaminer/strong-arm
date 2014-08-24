771bdb207f93e81294b8dfc308cb96af6722b4d8
fpgaminer <fpgaminer@bitcoin-mining.com>
2014-08-23 21:34:33 -0700

    HMAC now uses HMAC_partial internally.  Added unit test for HMAC_partial.

--- src/hmac/hmac.c
+++ src/hmac/hmac.c
@@ -5,30 +5,9 @@
 
 void HMAC (uint8_t *const hash, uint8_t const *const key, uint32_t klen, uint8_t const *const msg, uint32_t mlen)
 {
-	SHA256_STATE sha256_state = {0};
-	uint8_t padded_key[64] = {0};
-	
-	/* If the key is longer than the SHA256 block size, hash it and pad with 0s.
-	 * Otherwise, just pad with 0s. */
-	if (klen > 64)
-		SHA256 (padded_key, key, klen);
-	else
-		memmove (padded_key, key, klen);
-	
-	/* Inner hash */
-	for (uint32_t i = 0; i < 64; ++i)
-		padded_key[i] ^= 0x36;
-	SHA256_partial (0, &sha256_state, padded_key, 64, true, false);
-	SHA256_partial (hash, &sha256_state, msg, mlen, false, true);
-	
-	/* Outer hash */
-	/* We XOR with 0x36 to remove the previous XOR */
-	for (uint32_t i = 0; i < 64; ++i)
-		padded_key[i] ^= 0x36 ^ 0x5C;
-	SHA256_partial (NULL, &sha256_state, padded_key, 64, true, false);
-	SHA256_partial (hash, &sha256_state, hash, 32, false, true);
-	
-	/* Result is now in (hash) */
+	HMAC_STATE state;
+
+	HMAC_partial (hash, &state, key, klen, msg, mlen, true, true);
 }
 
 
