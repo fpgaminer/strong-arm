cef6ab0a32ef3dffad97ef27cc9b0b3f39bb428f
fpgaminer <fpgaminer@bitcoin-mining.com>
2014-08-23 19:03:43 -0700

    Added HMAC_partial for calculating HMAC of data over the course of multiple function calls.

--- include/strong-arm/hmac.h
+++ include/strong-arm/hmac.h
@@ -5,11 +5,30 @@
 #ifndef __HMAC_H__
 #define __HMAC_H__
 
+#include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <strong-arm/sha256.h>
+
+
+typedef struct {
+	SHA256_STATE sha256_state;
+	uint8_t padded_key[64];
+} HMAC_STATE;
+
 
 
 /* Write the HMAC hash of (msg) of length (mlen) with key (key) of length (klen) to (hash).
  * (hash) should be at least 32 bytes. */
 void HMAC (uint8_t *const hash, uint8_t const *const key, uint32_t klen, uint8_t const *const msg, uint32_t mlen);
+
+
+/* Call this method repeatedly with chunks of data to calculate the HMAC hash of all the data.
+ * The first call must set 'first' to true.  The last call must set 'last' to true.
+ * The 256-bit result is not written to hash until the last call.
+ * It is valid to set both first and last to true.
+ * All of key must be provided on the first call, but not on any subsequent calls.
+ */
+void HMAC_partial (void *hash, HMAC_STATE *const state, void const *const key, size_t klen, void const *msg, size_t mlen, bool const first, bool const last);
 
 #endif
--- src/hmac/hmac.c
+++ src/hmac/hmac.c
@@ -30,3 +30,40 @@ void HMAC (uint8_t *const hash, uint8_t const *const key, uint32_t klen, uint8_t
 	
 	/* Result is now in (hash) */
 }
+
+
+void HMAC_partial (void *hash, HMAC_STATE *const state, void const *const key, size_t klen, void const *msg, size_t mlen, bool const first, bool const last)
+{
+	if (first)
+	{
+		memset (state, 0, sizeof (HMAC_STATE));
+	
+		/* If the key is longer than the SHA256 block size, hash it and pad with 0s.
+		 * Otherwise, just pad with 0s. */
+		if (klen > 64)
+			SHA256 (state->padded_key, key, klen);
+		else
+			memmove (state->padded_key, key, klen);
+	
+		/* Inner hash */
+		for (uint32_t i = 0; i < 64; ++i)
+			state->padded_key[i] ^= 0x36;
+
+		SHA256_partial (0, &(state->sha256_state), state->padded_key, 64, true, false);
+	}
+
+	/* Add msg */
+	SHA256_partial (hash, &(state->sha256_state), msg, mlen, false, last);
+
+	if (!last)
+		return;
+	
+	/* Outer hash */
+	/* We XOR with 0x36 to remove the previous XOR */
+	for (uint32_t i = 0; i < 64; ++i)
+		state->padded_key[i] ^= 0x36 ^ 0x5C;
+	SHA256_partial (NULL, &(state->sha256_state), state->padded_key, 64, true, false);
+	SHA256_partial (hash, &(state->sha256_state), hash, 32, false, true);
+	
+	/* Result is now in (hash) */
+}
